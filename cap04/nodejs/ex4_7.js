2512a7b9d97ddecf50785641682702dd60e20841
Gustavo-SLemos <gustavo.lemossc@gmail.com>
2023-04-24 16:37:03 -0300

    exercício aplicado com nodejs

---
+++ cap04/nodejs/ex4_7.js
@@ -0,0 +1,13 @@
+// A entrada para um clube de pesca custa R$ 20,00 por pessoa e cada pessoa tem direito a levar um peixe. Peixes extras custam 12,00. Elabore um programa que leia o número de pessoas de uma família que foram ao clube e o número de peixes obtidos na pescaria. Informe o valor a pagar.
+
+const prompt = require("prompt-sync")(); //adiciona o pacote prompt-sync
+const pessoas = Number(prompt("Número de pessoas: ")); //lê dados de entrada
+const peixes = Number(prompt("Número de peixes: ")); //lê dados de entrada
+let pagar
+if (peixes <= pessoas) {
+    pagar = pessoas * 20
+}
+else {
+    pagar = (pessoas * 20) + ((peixes - pessoas)*12)
+}
+console.log(`Pagar R$: ${pagar.toFixed(2)}`)
