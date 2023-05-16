0b63e0d44106a3baaea7cd8650bf2a732e05709e
Gustavo-SLemos <gustavo.lemossc@gmail.com>
2023-05-16 18:04:03 -0300

    Código ajustado para otimizar a performance ex5_6

--- cap05/JS/ex5_6.js
+++ cap05/JS/ex5_6.js
@@ -9,7 +9,7 @@ frm.addEventListener("submit", (e) => {
 
     const num = Number(frm.inNumero.value)
 
-    let numDivisores = 0 //declara e inicializa o contador
+/*    let numDivisores = 0 //declara e inicializa o contador
     for (let i = 1; i <= num; i++) { //percorre todos os possíveis divisores de num
         if (num % i == 0) {
 
@@ -21,4 +21,20 @@ frm.addEventListener("submit", (e) => {
 } else {
     resp.innerText = `${num} não é primo`
 }
-})
+}) */
+
+let  temDivisor = 0
+
+for (let i = 2;  i <= num / 2; i++) {
+    if (num % i == 0) {
+        temDivisor = 1
+        break
+    }
+}
+
+if (num > 1 && !temDivisor) {
+    resp.innerText = `${num} é primo`
+} else {
+    resp.innerText = `${num} não é primo`
+}
+});
